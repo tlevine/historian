9abde780ebdd02b2dd72881f8f17dc154ec0faaf
Thomas Levine <_@thomaslevine.com>
2014-08-28 21:16:27 +0000

    catch shell error things

--- historian_reader/main.py
+++ historian_reader/main.py
@@ -3,14 +3,20 @@ import csv
 
 import historian_reader.shell
 
+
 def main():
     writer = csv.writer(sys.stdout)
     writer.writerow(('session','session_date','command_id','command_date','token'))
-    for session in historian_reader.shell.historian():
-        for token in session['tokens']:
-            command_id, command_date, token_string = token
-            writer.writerow((session['session'],
-                             session['session_date'].timestamp(),
-                             command_id,
-                             command_date.timestamp(),
-                             token_string))
+    try:
+        for session in historian_reader.shell.historian():
+            for token in session['tokens']:
+                command_id, command_date, token_string = token
+                writer.writerow((session['session'],
+                                 session['session_date'].timestamp(),
+                                 command_id,
+                                 command_date.timestamp(),
+                                 token_string))
+    except KeyboardInterrupt:
+        pass
+    except BrokenPipeError:
+        pass
