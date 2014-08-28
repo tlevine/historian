0c5c97eee2a66438b6ccc38bcc4216db31221cd1
Thomas Levine <_@thomaslevine.com>
2014-08-28 21:14:18 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='historian',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Parse Tom\'s history files',
+      url='http://small.dada.pink/historian',
+      packages=['historian_reader'],
+      tests_require = ['nose'],
+      scripts = ['read-history'],
+      version='0.0.1',
+      license='AGPL',
+)
